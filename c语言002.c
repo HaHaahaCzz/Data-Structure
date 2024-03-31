70fa404d6d66bedd12dfbaec0ee0b958fa74a30f
HaHaahaCzz <148534467+HaHaahaCzz@users.noreply.github.com>
2024-03-31 16:33:02 +0800

    Add files via upload

---
+++ c语言002.c
@@ -0,0 +1,77 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct person {
+	int age;
+	char name[10];
+	struct person* next;
+	struct person* last;
+}person_list;
+
+person_list* create(int n) {
+	person_list* head, * node, * end;
+	head = (person_list*)malloc(sizeof(person_list));
+	scanf_s("%d", &head->age);
+	scanf_s("%s", &head->name, 9);
+	end = head;
+	for (int i = 0; i < n-1; i++) {
+		node = (person_list*)malloc(sizeof(person_list));
+		scanf_s("%d", &node->age);
+		scanf_s("%s", &node->name, 9);
+		node->last = end;
+		end->next = node;
+		end = node;
+	}
+	end->next = head;
+	head->last = end;
+	return head;
+}
+
+void anti_get(person_list* head, int n) {
+	person_list* t = head;
+	for (int i = 0; i < n; i++) {
+		t = t->last;
+	}
+	if (t != NULL) {
+		printf("%d", t->age);
+		printf("%s", t->name);
+	}
+}
+
+int main()
+{
+	person_list* people = create(4);
+	anti_get(people, 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
